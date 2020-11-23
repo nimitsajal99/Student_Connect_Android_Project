59390f6303c8ed7fe0efd1f0f8a739eb7f75edea
nimitsajal <nimitsajaal99@gmail.com>
2020-11-23 20:56:27 +0530

    Seventh Commit.

--- app/src/main/java/com/nimitsajal/studentconnectapp/mainFeed.kt
+++ app/src/main/java/com/nimitsajal/studentconnectapp/mainFeed.kt
@@ -1,26 +1,28 @@
 package com.nimitsajal.studentconnectapp
 
+import android.R.attr.bitmap
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.muddzdev.styleabletoast.StyleableToast
+import com.google.firebase.firestore.auth.User
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main_feed.*
-import kotlinx.android.synthetic.main.current_chat_adapter.view.*
-import kotlinx.android.synthetic.main.toast_login_adapter.*
-import kotlinx.android.synthetic.main.toast_text_adapter.*
-import kotlinx.android.synthetic.main.post_adapter.*
-import kotlinx.android.synthetic.main.post_adapter.view.*
+import kotlinx.android.synthetic.main.post_adapter_cardiew.view.*
+
 
 class mainFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +32,7 @@ class mainFeed : AppCompatActivity() {
         val adapter = GroupAdapter<GroupieViewHolder>()
 
         var arrayPost = mutableListOf<postList>()
+        var arraySearch: MutableList<usersList> = mutableListOf<usersList>()
 
         var username = intent.getStringExtra("username")
         val db = FirebaseFirestore.getInstance()
@@ -41,7 +44,8 @@ class mainFeed : AppCompatActivity() {
                 if(result != null){
                     username = result.getString("Username").toString()
                     Log.d("profilePage", username.toString())
-                    loadFeed(arrayPost, adapter, username!!, db)
+                    //loadFeed(arrayPost, adapter, username!!, db)
+                    loadSearch(username!!, db, adapter, arraySearch)
                 }
                 else{
                     Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
@@ -99,7 +103,38 @@ class mainFeed : AppCompatActivity() {
         moveTaskToBack(true)
     }
 
-    private fun loadFeed(arrayPost: MutableList<postList>, adapter: GroupAdapter<GroupieViewHolder>, username: String, db: FirebaseFirestore){
+    private fun loadSearch(username: String, db: FirebaseFirestore, adapter: GroupAdapter<GroupieViewHolder>, arraySearch: MutableList<usersList>){
+        db.collection("Users")
+            .addSnapshotListener { value, error ->
+                if(value == null || error != null){
+                    Toast.makeText(this, "ERRRRRRRRROR", Toast.LENGTH_SHORT).show()
+                    return@addSnapshotListener
+                }
+                for(document in value.documents){
+                    if(document.id != "Info" && document.id != username){
+                        val temp = usersList(document.id, "", document["Name"].toString(), document["Picture"].toString())
+                        arraySearch.add(temp)
+                        adapter.add(UserItemSearch(document.id, document["Picture"].toString(), document["Name"].toString()))
+                    }
+                }
+                adapter.setOnItemClickListener { item, view ->
+                    val searchItem: UserItemSearch = item as UserItemSearch
+                    val to = searchItem.username
+                    val intent = Intent(this, others_profile_page::class.java)
+                    intent.putExtra("usernameOthers", to)
+                    startActivity(intent)
+                    finish()
+                }
+                rvFeed.adapter = adapter
+            }
+    }
+
+    private fun loadFeed(
+        arrayPost: MutableList<postList>,
+        adapter: GroupAdapter<GroupieViewHolder>,
+        username: String,
+        db: FirebaseFirestore
+    ){
         val user = db.collection("Users").document(username).collection("My Feed")
         user
             .orderBy("Time", Query.Direction.ASCENDING)
@@ -113,8 +148,20 @@ class mainFeed : AppCompatActivity() {
                     db.collection("Post").document(document.id)
                         .get()
                         .addOnSuccessListener {
-                            val temp = postList(it["From"].toString(), it["Picture"].toString(), it["Dp"].toString(), it["Description"].toString())
-                            adapter.add(post_class(it["From"].toString(), it["Picture"].toString(), it["Dp"].toString(), it["Description"].toString()))
+                            val temp = postList(
+                                it["From"].toString(),
+                                it["Picture"].toString(),
+                                it["Dp"].toString(),
+                                it["Description"].toString()
+                            )
+                            adapter.add(
+                                post_class(
+                                    it["From"].toString(),
+                                    it["Picture"].toString(),
+                                    it["Dp"].toString(),
+                                    it["Description"].toString()
+                                )
+                            )
                         }
                 }
             }
@@ -123,19 +170,77 @@ class mainFeed : AppCompatActivity() {
     }
 }
 
-data class postList(var username: String, var imageUrl: String, var dpUrl: String, var description: String){
+data class postList(
+    var username: String,
+    var imageUrl: String,
+    var dpUrl: String,
+    var description: String
+){
 }
 
-class post_class(var username: String, var imageUrl: String, var dpUrl: String, var description: String): Item<GroupieViewHolder>(){
+class post_class(
+    var username: String,
+    var imageUrl: String,
+    var dpUrl: String,
+    var description: String
+): Item<GroupieViewHolder>(){
     @SuppressLint("RestrictedApi")
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.itemView.tvUsername.text = username
-        viewHolder.itemView.tvDescription.text = description
-        Picasso.get().load(dpUrl).into(viewHolder.itemView.circularImageView)
-        Picasso.get().load(imageUrl).into(viewHolder.itemView.postImage)
+        viewHolder.itemView.tvUsernameCard.text = username
+        viewHolder.itemView.tvDescriptionCard.text = description
+        Picasso.get().load(dpUrl).into(viewHolder.itemView.circularImageViewCard)
+        Picasso.get().load(imageUrl).into(viewHolder.itemView.postImageCard)
+
+
+
+//        Picasso.get().load(imageUrl).into(object : com.squareup.picasso.Target {
+//            @RequiresApi(Build.VERSION_CODES.O)
+//            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+//                // loaded bitmap is here (bitmap)
+//                Log.d("colorset", "bitmap loaded")
+//
+//                if (bitmap != null) {
+//                    Palette.Builder(bitmap).generate { it?.let {  palette ->
+//                        val vibrant: Int = palette.getVibrantColor(0x000000) // <=== color you want
+//                        val vibrantLight: Int = palette.getLightVibrantColor(0x000000)
+//                        val vibrantDark: Int = palette.getDarkVibrantColor(0x000000)
+//                        val muted: Int = palette.getMutedColor(0x000000)
+//                        val mutedLight: Int = palette.getLightMutedColor(0x000000)
+//                        val mutedDark: Int = palette.getDarkMutedColor(0x000000)
+//                        val dominant: Int = palette.getDominantColor(0x000000)
+//
+//                        viewHolder.itemView.cvBehindImage.setCardBackgroundColor(muted)
+//                        Log.d("colorset", "color set $muted")
+//
+//                    } }
+//                }
+//
+////                Palette.from(android.R.attr.bitmap).generate { palette ->
+////                    val vibrant: Int = palette.getVibrantColor(0x000000) // <=== color you want
+////                    val vibrantLight: Int = palette.getLightVibrantColor(0x000000)
+////                    val vibrantDark: Int = palette.getDarkVibrantColor(0x000000)
+////                    val muted: Int = palette.getMutedColor(0x000000)
+////                    val mutedLight: Int = palette.getLightMutedColor(0x000000)
+////                    val mutedDark: Int = palette.getDarkMutedColor(0x000000)
+////                }
+////
+////                var newBitmap: Bitmap? = bitmap?.let { Bitmap.createScaledBitmap(it, 1, 1, true) };
+////                var color = newBitmap?.getPixel(0, 0);
+////                newBitmap?.recycle();
+////                if (color != null) {
+////                    viewHolder.itemView.cvBehindImage.setCardBackgroundColor(color)
+////                    Log.d("colorset", "color set ${color}")
+////                }
+//            }
+//
+//            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+//
+//            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {}
+//        })
     }
 
     override fun getLayout(): Int {
-        return R.layout.post_adapter
+        return R.layout.post_adapter_cardiew
     }
 }
+
---
+++ app/src/main/java/com/nimitsajal/studentconnectapp/others_profile_page.kt
@@ -0,0 +1,389 @@
+package com.nimitsajal.studentconnectapp
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_main_feed.*
+import kotlinx.android.synthetic.main.activity_others_profile_page.*
+import kotlinx.android.synthetic.main.activity_profile_page.*
+import kotlinx.android.synthetic.main.details_adapter.view.*
+
+class others_profile_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_others_profile_page)
+
+        var username: String = ""
+        var isFriend: Boolean = false
+        var friend: isFriend = isFriend(false, 0, "", "")
+
+        var usernameOthers = intent.getStringExtra("usernameOthers")
+        tvUsernameOthers.setText("").toString()
+        //tvUsername.setText(username).toString()
+
+
+        tvDescriptionOthers.setText("").toString()
+
+        tvNameOthers.setText("").toString()
+
+        btnFriendsOthers.setText("").toString()
+
+        btnMessageOthers.setText("Message").toString()
+
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        if(user != null){
+            val user_table = db.collection("User Table").document(user.uid.toString())
+            user_table.get().addOnSuccessListener { result ->
+                if(result != null){
+                    username = result.getString("Username").toString()
+                    Log.d("profilePage", username.toString())
+                    if (usernameOthers != null) {
+                        db.collection("Users").document(username).collection("Friends")
+                            .document(usernameOthers)
+                            .get()
+                            .addOnCompleteListener {
+                                if(it.isSuccessful){
+                                    val temp = it.getResult()
+                                    if (temp != null) {
+                                        if(temp.exists()){
+                                            btnFriendsOthers.text = "Unfriend"
+                                            friend.isFriend = true
+                                            //Toast.makeText(this, "isFriend = true", Toast.LENGTH_SHORT).show()
+                                        }
+                                        else{
+                                            btnFriendsOthers.text = "Add Friend"
+                                            friend.isFriend = false
+                                            //Toast.makeText(this, "isFriend = false", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                }
+                                getUser(auth,usernameOthers, db,friend)
+                            }
+                    }
+                }
+                else{
+                    Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
+                    return@addOnSuccessListener
+                }
+            }
+        }
+
+        btnChatOthers.setOnClickListener {
+            val intent = Intent(this, currentChats::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+
+        btnLogoutOthers.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            finish()
+        }
+
+        btnProfileOthers.setOnClickListener {
+            val intent = Intent(this, profilePage::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+
+        btnFeedOthers.setOnClickListener {
+            val intent = Intent(this, mainFeed::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+
+        btnFriendsOthers.setOnClickListener {
+            if (usernameOthers != null && username != null) {
+                btnFriendsOthers.isEnabled = false
+                friend(username, usernameOthers, db, friend)
+            }
+        }
+
+        btnMessageOthers.setOnClickListener {
+            if (usernameOthers != null && username != null){
+                btnMessageOthers.isEnabled = false
+                chat(username, db, friend, usernameOthers)
+            }
+        }
+    }
+
+    private fun friend(username: String, usernameOthers: String, db: FirebaseFirestore, friend: isFriend){
+        if(friend.isFriend){
+            db.collection("Users").document(username).collection("Friends").document(usernameOthers)
+                .delete()
+                .addOnCompleteListener{
+                    if(it.isSuccessful){
+                        Log.d("others", "userOther removed from user")
+                    }
+                }
+            db.collection("Users").document(usernameOthers).collection("Friends").document(username)
+                .delete()
+                .addOnCompleteListener {it2 ->
+                    if(it2.isSuccessful){
+                        Toast.makeText(this, "$usernameOthers is no longer a Friend", Toast.LENGTH_SHORT).show()
+                        friend.isFriend = false
+                        friend.count = friend.count-1
+                        val str = "Add Friend (" + friend.count + ")"
+                        btnFriendsOthers.text = str
+                        btnFriendsOthers.isEnabled = true
+                    }
+                }
+        }
+        else{
+            var dpUsername = ""
+            var name = ""
+            db.collection("Users").document(username)
+                .get()
+                .addOnSuccessListener {
+                    if(it != null){
+                        dpUsername = it.getString("Picture").toString()
+                        name = it.getString("Name").toString()
+                        Log.d("others", "user name = $name, dp = $dpUsername")
+                                    val user = hashMapOf(
+                                        "Picture" to dpUsername,
+                                        "Name" to name
+                                    )
+                                    val userOthers = hashMapOf(
+                                        "Picture" to friend.url,
+                                        "Name" to friend.name
+                                    )
+                                    db.collection("Users").document(username).collection("Friends").document(usernameOthers)
+                                        .set(userOthers)
+                                        .addOnCompleteListener {it3 ->
+                                            if(it3.isSuccessful){
+                                                Log.d("others", "user other added in user")
+                                            }
+                                            else{
+                                                Log.d("others", "inner user not entered = $it3")
+                                            }
+                                        }
+                                    db.collection("Users").document(usernameOthers).collection("Friends").document(username)
+                                        .set(user)
+                                        .addOnCompleteListener {it4 ->
+                                            if(it4.isSuccessful){
+                                                Toast.makeText(this, "${friend.name} added as a Friend", Toast.LENGTH_SHORT).show()
+                                                friend.isFriend = true
+                                                friend.count = friend.count+1
+                                                val str = "Unfriend (" + friend.count + ")"
+                                                btnFriendsOthers.text = str
+                                                btnFriendsOthers.isEnabled = true
+                                            }
+                                        }
+                                        .addOnFailureListener {it5 ->
+                                            Log.d("others", "outer user not entered = ${it5.message}")
+                                        }
+                                }
+                }
+        }
+    }
+
+    //TODO: search if document exists optimisation
+    private fun chat(username: String, db: FirebaseFirestore, friend: isFriend, usernameOthers: String){
+        db.collection("Users").document(username).collection("Chats").document(usernameOthers)
+            .get()
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    val temp = it.getResult()
+                    if (temp != null) {
+                        if(temp.exists()){
+                            val intent = Intent(this, chat::class.java)
+                            intent.putExtra("from", username)
+                            intent.putExtra("to", usernameOthers)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Log.d("others", "inner")
+                            newChat(username, db, friend, usernameOthers)
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun newChat(username: String, db: FirebaseFirestore, friend: isFriend, usernameOthers: String){
+        val message = hashMapOf(
+            "From" to "System",
+            "To" to usernameOthers,
+            "Text" to "Say Hi",
+            "Time" to FieldValue.serverTimestamp()
+        )
+
+        val messageFrom = hashMapOf(
+            "From" to "System",
+            "To" to usernameOthers,
+            "Text" to "Say Hi",
+            "Name" to usernameOthers,
+            "Time" to FieldValue.serverTimestamp()
+        )
+
+        val messageTo = hashMapOf(
+            "From" to "System",
+            "To" to usernameOthers,
+            "Text" to "Say Hi",
+            "Name" to username,
+            "Time" to FieldValue.serverTimestamp()
+        )
+
+        val info = hashMapOf(
+            "Info" to "Info"
+        )
+
+        db.collection("Users").document(username).collection("Chats").document(usernameOthers)
+            .set(messageFrom)
+            .addOnCompleteListener{
+                if(it.isSuccessful)
+                {
+                    db.collection("Users").document(username).collection("Chats").document(usernameOthers).collection("Next")
+                        .add(message)
+                        .addOnCompleteListener{it1->
+                            if(it1.isSuccessful)
+                            {
+                                Log.d("others", "chat created in user")
+                            }
+
+                        }
+                }
+            }
+        db.collection("Users").document(usernameOthers).collection("Chats").document(username)
+            .set(messageTo)
+            .addOnCompleteListener{it2->
+                if(it2.isSuccessful)
+                {
+                    db.collection("Users").document(usernameOthers).collection("Chats").document(username).collection("Next")
+                        .add(message)
+                        .addOnCompleteListener{it3->
+                            if(it3.isSuccessful)
+                            {
+                                val intent = Intent(this, chat::class.java)
+                                intent.putExtra("from", username)
+                                intent.putExtra("to", usernameOthers)
+                                startActivity(intent)
+                                btnMessageOthers.isEnabled = true
+                                finish()
+                            }
+
+                        }
+                }
+
+            }
+    }
+
+    private fun getUser(auth: FirebaseAuth, username: String, db: FirebaseFirestore, friend: isFriend){
+
+
+        tvUsernameOthers.setText(username.toString()).toString()
+
+        val user_info = db.collection("Users").document(username)
+        user_info.get().addOnSuccessListener {
+            if(it != null){
+
+
+                tvNameOthers.setText(it.getString("Name").toString()).toString()
+                friend.name = it.getString("Name").toString()
+
+
+                if(it.getString("Description").toString() == ""){
+                    collapse(tvDescriptionOthers)
+//                    tvDescription.setText(it.getString("Description").toString()).toString()
+//                    expand(tvName, 999)
+                }
+                else{
+                    expand(tvDescriptionOthers, 999)
+                    tvDescriptionOthers.setText(it.getString("Description").toString()).toString()
+                }
+                val arrayDetails = mutableListOf<String>()
+                val adapter = GroupAdapter<GroupieViewHolder>()
+                db.collection("Users").document(username).collection("Medals")
+                    .get()
+                    .addOnSuccessListener {
+                        for(document in it){
+                            arrayDetails.add(document.id.toString())
+                        }
+                        for(i in 0 until arrayDetails.size){
+                            var num = (0 until arrayDetails.size).random()
+                            adapter.add(details_class(arrayDetails.get(num)))
+                            arrayDetails.removeAt(num)
+                        }
+                        rvDetailsOthers.adapter = adapter
+                    }
+
+                var selectedPhotoUrl_string = it.getString("Picture").toString()
+                val selectedPhotoUrl = Uri.parse(selectedPhotoUrl_string)
+//                val selectedPhotoUrl = selectedPhotoUrl_string.toUri()
+//                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUrl)
+//                circularImageView.setImageBitmap(bitmap)
+
+                friend.url = selectedPhotoUrl_string
+
+
+                Picasso.get().load(selectedPhotoUrl_string).into(object :
+                    com.squareup.picasso.Target {
+                    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                        // loaded bitmap is here (bitmap)
+                        circularImageViewOthers.setImageBitmap(bitmap)
+                    }
+
+                    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+
+                    override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {}
+                })
+            }
+        }
+
+
+
+        var friends = ""
+        if(friend.isFriend == true){
+            friends = "Unfriend ("
+        }
+        else{
+            friends = "Add Friend ("
+        }
+
+        val FriendsCount = user_info.collection("Friends").get()
+            .addOnSuccessListener {
+                if(it != null){
+                    friend.count = it.size() - 1
+                    friends = friends + friend.count + ")"
+                    btnFriendsOthers.setText(friends).toString()
+                }
+            }
+    }
+
+    private fun collapse(textView: TextView) {
+        textView.height = 0
+    }
+    private fun expand(textView: TextView, height: Int) {
+        if(height == 999){
+            textView.layoutParams =
+                LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+        }
+    }
+}
+
+data class isFriend(var isFriend: Boolean, var count: Int, var name: String, var url: String){
+
+}
